6e5d0d2038cb6aa30f2337bc39c85f31889044a0
Javier Terán González <javiteran@hotmail.com>
2022-07-30 19:58:25 +0200

    cambiar mensaje

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!. https://aws.amazon.com/es/getting-started/hands-on/create-continuous-delivery-pipeline/'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
